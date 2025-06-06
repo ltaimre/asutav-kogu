5bcfc8761979df0b8863d4fe7a9430539f3fb370
liisit <ltaimre@tlu.ee>
2025-06-06 21:39:32 +0300

    🎉 Init Next.js project with Chakra UI and clean structure

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import { Box, Heading } from '@chakra-ui/react'
+
+const Header = () => (
+  <Box bg="teal.500" p={4}>
+    <Heading color="white">Minu Rakendus</Heading>
+  </Box>
+)
+
+export default Header
